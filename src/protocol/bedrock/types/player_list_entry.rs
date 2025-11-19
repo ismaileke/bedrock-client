9476d41d3173a9f954cd5fa4fcdcfed65ce69e49
ismail <dev.1smail.eke34@gmail.com>
2025-11-19 19:03:46 +0300

    player_list.rs incorrect variable locations (bad error)

--- src/protocol/bedrock/types/player_list_entry.rs
+++ src/protocol/bedrock/types/player_list_entry.rs
@@ -8,10 +8,10 @@ pub struct PlayerListEntry {
     pub uuid: String,
     pub actor_unique_id: i64,
     pub username: String,
-    pub skin_data: SkinData,
     pub xbox_user_id: String,
     pub platform_chat_id: String,
     pub build_platform: i32,
+    pub skin_data: SkinData,
     pub is_teacher: bool,
     pub is_host: bool,
     pub is_sub_client: bool,
@@ -26,10 +26,10 @@ impl PlayerListEntry {
             uuid,
             actor_unique_id: 0,
             username: String::new(),
-            skin_data: SkinData::default(String::new(), String::new(), String::new(), SkinImage::new(0, 0, String::new())),
             xbox_user_id: String::new(),
             platform_chat_id: String::new(),
             build_platform: DeviceOS::UNKNOWN,
+            skin_data: SkinData::default(String::new(), String::new(), String::new(), SkinImage::new(0, 0, String::new())),
             is_teacher: false,
             is_host: false,
             is_sub_client: false,
@@ -41,15 +41,15 @@ impl PlayerListEntry {
         uuid: String,
         actor_unique_id: i64,
         username: String,
-        skin_data: SkinData,
         xbox_user_id: String,
         platform_chat_id: String,
         build_platform: i32,
+        skin_data: SkinData,
         is_teacher: bool,
         is_host: bool,
         is_sub_client: bool,
         color: Option<Color>,
     ) -> PlayerListEntry {
-        PlayerListEntry { uuid, actor_unique_id, username, skin_data, xbox_user_id, platform_chat_id, build_platform, is_teacher, is_host, is_sub_client, color }
+        PlayerListEntry { uuid, actor_unique_id, username, xbox_user_id, platform_chat_id, build_platform, skin_data, is_teacher, is_host, is_sub_client, color }
     }
 }
